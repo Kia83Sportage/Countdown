ce7eabe9091e26e68ce6c45ed80346f598e23590
mscha <m.saleh.sedeghi@gmail.com>
2024-05-09 01:10:19 +0330

    random number and timer

--- app/src/main/java/com/example/countdown/MainActivity.java
+++ app/src/main/java/com/example/countdown/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.countdown;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView numHour;
+    TextView numMin;
+    TextView numSec;
+
+    Random random = new Random();
+
+    Integer hour = random.nextInt(23);
+    Integer minutes = random.nextInt(59);
+    Integer second = random.nextInt(59);
+
+    Handler handler = new Handler();
+    Runnable runnable;
+    int delay = 1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +40,55 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        numHour = findViewById(R.id.num_hour);
+        numMin = findViewById(R.id.num_min);
+        numSec = findViewById(R.id.num_sec);
+
+        numSec.setText(String.valueOf(second));
+        numMin.setText(String.valueOf(minutes));
+        numHour.setText(String.valueOf(hour));
+    }
+
+    @Override
+    protected void onResume() {
+        handler.postDelayed(runnable = () -> {
+            handler.postDelayed(runnable, delay);
+            if (second > 0){
+                second -= 1;
+                if (second > 9) {
+                    numSec.setText(String.valueOf(second));
+                }else {
+                    numSec.setText("0" + String.valueOf(second));
+                }
+            } else if (minutes > 0){
+                second = 59;
+                minutes -= 1;
+                numSec.setText(String.valueOf(second));
+                if (minutes > 9) {
+                    numMin.setText(String.valueOf(minutes));
+                }else {
+                    numMin.setText("0" + String.valueOf(minutes));
+                }
+                numMin.setText(String.valueOf(minutes));
+            } else if (hour > 0){
+                second = 59;
+                minutes = 59;
+                hour -= 1;
+                numSec.setText(String.valueOf(second));
+                numMin.setText(String.valueOf(minutes));
+                if (hour > 9) {
+                    numHour.setText(String.valueOf(hour));
+                }else {
+                    numHour.setText("0" + String.valueOf(hour));
+                }
+            } else {
+                Toast toast = Toast.makeText(MainActivity.this, "شما مردید", Toast.LENGTH_LONG);
+                toast.show();
+                handler.removeCallbacks(runnable);
+            }
+
+        }, delay);
+        super.onResume();
     }
 }
